63a015473474730a33615c1ba9f5d55be48aca74
Margus Lanto <margus.lanto@frontify.com>
2024-10-24 15:25:39 +0200

    Add asset update mutation

--- examples/bulk-actions-app/src/App.tsx
+++ examples/bulk-actions-app/src/App.tsx
@@ -1,6 +1,6 @@
 import { AppBridgePlatformApp } from '@frontify/app-bridge-app';
 import { useEffect, useState } from 'react';
-import { getAssetsByIds } from './helpers/graphql';
+import { getAssetsByIds, updateAssetTitle } from './helpers/graphql';
 import { Button } from '@frontify/fondue/components';
 import { Heading, Stack, Text, TextInput } from '@frontify/fondue';
 
@@ -26,27 +26,72 @@ const highlightMatches = (filename: string, query: string) => {
     return { highlightedText, matchCount };
 };
 
-const getResultCount = (count: number) => {
-    if (count === 0) {
+const getResultCount = (matchCount: number, assetCount: number) => {
+    if (matchCount === 0) {
         return 'No matches';
     }
 
-    return `${count} match${count === 1 ? '' : 'es'}`;
+    return `${matchCount} match${matchCount === 1 ? '' : 'es'} in ${assetCount} asset${assetCount === 1 ? '' : 's'}`;
 };
+
+type Asset = { id: string; previewUrl: string; title: string; extension: string };
 
 export const App = () => {
     const appBridge = new AppBridgePlatformApp();
     const context = appBridge.context().get();
 
-    const [assets, setAssets] = useState<{ id: string; previewUrl: string; title: string; extension: string }[]>([]);
+    const [assets, setAssets] = useState<Asset[]>([]);
     const [findText, setFindText] = useState('');
     const [replaceText, setReplaceText] = useState('');
     const [matchCount, setMatchCount] = useState(0);
+    const [matchingAssets, setMatchingAssets] = useState<Asset[]>([]);
+    const [renamingInProgress, setRenamingInProgress] = useState(false);
+    const [progressMessage, setProgressMessage] = useState('');
 
     const assetsAreFetched = assets.length > 0;
+    const matchingAssetCount = matchingAssets.length;
 
-    const renameAssets = () => {
-        console.log('about to rename...');
+    const handleFindTextChange = (value: string) => {
+        setProgressMessage('');
+        setFindText(value);
+    };
+
+    const handleReplaceTextChange = (value: string) => {
+        setProgressMessage('');
+        setReplaceText(value);
+    };
+
+    const renameAssets = async () => {
+        const count = matchingAssetCount;
+        let index = 1;
+
+        setRenamingInProgress(true);
+
+        for (const asset of matchingAssets) {
+            setProgressMessage(`Renaming ${index} of ${count} asset${count === 1 ? '' : 's'}...`);
+
+            const newTitle = asset.title.replace(new RegExp(findText, 'gi'), replaceText);
+
+            try {
+                const response = await appBridge.api({
+                    name: 'executeGraphQl',
+                    payload: {
+                        query: updateAssetTitle(asset.id, newTitle),
+                    },
+                });
+
+                asset.title = response.updateAsset.asset.title;
+                index++;
+            } catch (error) {
+                console.error('Error renaming asset:', asset.id, error);
+            }
+
+            setAssets(assets);
+        }
+
+        setRenamingInProgress(false);
+        setMatchCount(0);
+        setProgressMessage(`Finished renaming ${count} asset${count === 1 ? '' : 's'}`);
     };
 
     useEffect(() => {
@@ -60,8 +105,6 @@ export const App = () => {
                 payload: { query: getAssetsByIds(context.selection.assets.ids) },
             });
 
-            console.log('response:', response);
-
             setAssets(response.assets);
         };
 
@@ -70,11 +113,20 @@ export const App = () => {
 
     useEffect(() => {
         let totalMatchCount = 0;
+        const matchedAssets: Asset[] = [];
+
         assets.forEach((asset) => {
             const { matchCount } = highlightMatches(asset.title, findText);
+
+            if (matchCount > 0) {
+                matchedAssets.push(asset);
+            }
+
             totalMatchCount += matchCount;
         });
+
         setMatchCount(totalMatchCount);
+        setMatchingAssets(matchedAssets);
     }, [findText, assets]);
 
     return (
@@ -89,25 +141,28 @@ export const App = () => {
                         placeholder="Find"
                         disabled={!assetsAreFetched}
                         value={findText}
-                        onChange={setFindText}
+                        onChange={handleFindTextChange}
                         onEnterPressed={() => {}}
                         onBlur={() => {}}
                     />
-                    <Text>{getResultCount(matchCount)}</Text>
+                    <Text>{getResultCount(matchCount, matchingAssetCount)}</Text>
                 </div>
-                <div className="tw-flex tw-gap-x-1">
+                <div className="tw-flex tw-gap-x-1 tw-items-center">
                     <TextInput
                         id="replace"
                         placeholder="Replace"
                         disabled={!assetsAreFetched}
                         value={replaceText}
-                        onChange={setReplaceText}
+                        onChange={handleReplaceTextChange}
                         onEnterPressed={() => {}}
                         onBlur={() => {}}
                     />
-                    <Button disabled={matchCount === 0} onPress={renameAssets}>
+                    <Button disabled={matchCount === 0 || renamingInProgress} onPress={renameAssets}>
                         Rename all
                     </Button>
+                </div>
+                <div className="tw-flex tw-h-4">
+                    <Text size="small">{progressMessage}</Text>
                 </div>
             </div>
             <div id="file-list">
